3c244b8412ef171b1f07b140d6bc1e960584953f
Vinicius Bezerra <viniciusg21bezerra@gmail.com>
2025-11-12 11:44:11 -0300

    validando formulario primeira parte

--- js/bytebank.js
+++ js/bytebank.js
@@ -3,3 +3,11 @@ let saldo = 3000;
 const elementoSaldo = document.querySelector('.saldo-valor .valor');
 elementoSaldo.textContent = `R$ ${saldo.toFixed(2)}`;
 
+const elementoFormulario = document.querySelector('.block-nova-transacao form');
+elementoFormulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return;
+    }
+});
