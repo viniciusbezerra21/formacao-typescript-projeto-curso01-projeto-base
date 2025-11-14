8a46fcd1db565229c0cc15f6c0ccb45e5cb5c464
Vinicius Bezerra <viniciusg21bezerra@gmail.com>
2025-11-14 15:28:45 -0300

    O typeScript entra no proximo commit

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,4 +1,4 @@
-let saldo = 3000;
+let saldo = 0;
 
 const elementoSaldo = document.querySelector('.saldo-valor .valor');
 elementoSaldo.textContent = `R$ ${saldo.toFixed(2)}`;
@@ -10,4 +10,32 @@ elementoFormulario.addEventListener('submit', function(event) {
         alert('Por favor, preencha todos os campos corretamente.');
         return;
     }
+
+    const inputTipoTransacao = document.querySelector('#tipoTransacao');
+    const inputValor = elementoFormulario.querySelector('#valor');
+    const inputData = elementoFormulario.querySelector('#data');
+
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if (tipoTransacao === 'Depósito') {
+        saldo += parseFloat(valor);
+    } else if (tipoTransacao === 'Transferência' || tipoTransacao === 'Pagamento de Boleto') {
+        saldo -= parseFloat(valor);
+    } else {
+        alert('Tipo de transação inválido.');
+        return;
+    }
+
+    elementoSaldo.textContent = `R$ ${saldo.toFixed(2)}`;
+
+
+    const novaTransacao = {
+        tipo: tipoTransacao,
+        valor: parseFloat(valor),
+        data: data
+    };
+
+    elementoFormulario.reset();
 });
